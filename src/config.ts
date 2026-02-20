97c1f02b82bc502198927b6caa47330540e514dc
halunhaku <123087543+forhalunhaku@users.noreply.github.com>
2026-02-20 14:18:24 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   website: "https://blog.halunhaku.top/", // replace this with your deployed domain
   author: "halunhaku ",
-  profile: "https://satnaing.dev/",
+  profile: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "halunhaku's blog",
   ogImage: "astropaper-og.jpg",
@@ -18,6 +18,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "zh", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
